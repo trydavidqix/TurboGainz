6cb1ee19c8e037aeef9d29a3396cd50b2f10a014
David William <davidwilliammacedo@icloud.com>
2025-05-25 12:54:21 +0100

    chore: configura scripts de deploy para GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TurboGainz/',
   plugins: [react()],
   resolve: {
     alias: {
